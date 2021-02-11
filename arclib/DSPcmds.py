8a664316b3d40ec786b2b9584c029d9b12497b77
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-02-11 11:50:10 -0700

    Move out old command map and use new one
    Hopefully final name changes in there

--- arclib/DSPcmds.py
+++ arclib/DSPcmds.py
@@ -10,10 +10,13 @@
 
 from __future__ import division, print_function, absolute_import
 
+# NOTE: This file actually contains the memory locations and contants
+#   that are used here; this is more of a skeleton in case we go back and
+#   can support ARC Gen II stuff and there are differences.
 import ARCGenIII as G3
 
 
-class ARCDSPCmd(object):
+class DSPCmd(object):
     # Just make it required to set each param ahead of time
     def __init__(self, goodname, prefix, nargs,
                  cmdtype=None, cmdloc=None, board=0, retval=None):
@@ -34,102 +37,89 @@ class ARCDSPCmd(object):
 #   board 1 is the pci card
 #   board 2 is the timing board
 #   board 3 is utility board
-ARC_command_list = dict()
+ARCCmds = dict()
 
-ARC_command_list["stop_idle"] = ARCDSPCmd('stop_idle', 'simple', 0,
+ARCCmds["stop_idle"] = DSPCmd('stop_idle', 'simple', 0,
+                              cmdtype=G3.ASTROPCI_COMMAND,
+                              cmdloc=G3.STP,
+                              board=2, retval="DON")
+
+ARCCmds["start_idle"] = DSPCmd('start_idle', 'simple', 0,
+                               cmdtype=G3.ASTROPCI_COMMAND,
+                               cmdloc=G3.IDL,
+                               board=2, retval="DON")
+
+ARCCmds["power_on"] = DSPCmd('power_on', 'simple', 0,
+                             cmdtype=G3.ASTROPCI_COMMAND,
+                             cmdloc=G3.PON,
+                             board=2, retval="DON")
+
+ARCCmds["power_off"] = DSPCmd('power_off', 'simple', 0,
+                              cmdtype=G3.ASTROPCI_COMMAND,
+                              cmdloc=G3.POF,
+                              board=2, retval="DON")
+
+ARCCmds["read_memory"] = DSPCmd('read_memory', 'simple', 1,
+                                cmdtype=G3.ASTROPCI_COMMAND,
+                                cmdloc=G3.RDM,
+                                board=0, retval="UNDEFINED")
+
+ARCCmds["write_memory"] = DSPCmd('write_memory', 'simple', 2,
+                                 cmdtype=G3.ASTROPCI_COMMAND,
+                                 cmdloc=G3.WRM,
+                                 board=0, retval="UNDEFINED")
+
+ARCCmds["set_amplifier"] = DSPCmd('set_amplifier', 'simple', 1,
+                                  cmdtype=G3.ASTROPCI_COMMAND,
+                                  cmdloc=G3.SOS,
+                                  board=2, retval="DON")
+
+ARCCmds["set_row_col"] = DSPCmd('set_row_col', 'simple', 2,
+                                cmdtype=G3.ASTROPCI_COMMAND,
+                                cmdloc=G3.SRC,
+                                board=2, retval="DON")
+
+ARCCmds["set_subframe_size"] = DSPCmd('set_subframe_size',
+                                      'simple', 3,
+                                      cmdtype=G3.ASTROPCI_COMMAND,
+                                      cmdloc=G3.SSS,
+                                      board=2, retval="DON")
+
+ARCCmds["set_subframe_position"] = DSPCmd('set_subframe_position',
+                                          'simple', 2,
                                           cmdtype=G3.ASTROPCI_COMMAND,
-                                          cmdloc=G3.STP,
+                                          cmdloc=G3.SSP,
                                           board=2, retval="DON")
 
-ARC_command_list["start_idle"] = ARCDSPCmd('start_idle', 'simple', 0,
-                                           cmdtype=G3.ASTROPCI_COMMAND,
-                                           cmdloc=G3.IDL,
-                                           board=2, retval="DON")
-
-ARC_command_list["power_on"] = ARCDSPCmd('power_on', 'simple', 0,
+ARCCmds["set_image_parameters"] = DSPCmd('set_image_parameters',
+                                         'simple', 4,
                                          cmdtype=G3.ASTROPCI_COMMAND,
-                                         cmdloc=G3.PON,
+                                         cmdloc=G3.SIP,
                                          board=2, retval="DON")
 
-ARC_command_list["power_off"] = ARCDSPCmd('power_off', 'simple', 0,
-                                          cmdtype=G3.ASTROPCI_COMMAND,
-                                          cmdloc=G3.POF,
-                                          board=2, retval="DON")
+ARCCmds["set_exposure_time"] = DSPCmd('set_exposure_time',
+                                      'simple', 1,
+                                      cmdtype=G3.ASTROPCI_COMMAND,
+                                      cmdloc=G3.SET,
+                                      board=2, retval="DON")
 
-ARC_command_list["read_memory"] = ARCDSPCmd('read_memory', 'simple', 1,
-                                            cmdtype=G3.ASTROPCI_COMMAND,
-                                            cmdloc=G3.RDM,
-                                            board=0, retval="UNDEFINED")
+ARCCmds["start_exposure"] = DSPCmd('start_exposure',
+                                   'simple', 0,
+                                   cmdtype=G3.ASTROPCI_COMMAND,
+                                   cmdloc=G3.SEX,
+                                   board=2, retval="DON")
 
-ARC_command_list["write_memory"] = ARCDSPCmd('write_memory', 'simple', 2,
-                                             cmdtype=G3.ASTROPCI_COMMAND,
-                                             cmdloc=G3.WRM,
-                                             board=0, retval="UNDEFINED")
+ARCCmds["clear_subframe"] = DSPCmd('clear_subframe',
+                                   'simple', 0,
+                                   cmdtype=G3.ASTROPCI_COMMAND,
+                                   cmdloc=G3.CSB,
+                                   board=2, retval="DON")
 
-ARC_command_list["set_amplifier"] = ARCDSPCmd('set_amplifier', 'simple', 1,
-                                              cmdtype=G3.ASTROPCI_COMMAND,
-                                              cmdloc=G3.SOS,
-                                              board=2, retval="DON")
-
-ARC_command_list["set_row_col"] = ARCDSPCmd('set_row_col', 'simple', 2,
-                                            cmdtype=G3.ASTROPCI_COMMAND,
-                                            cmdloc=G3.SRC,
-                                            board=2, retval="DON")
-
-ARC_command_list["set_subframe_size"] = ARCDSPCmd('set_subframe_size',
-                                                  'simple', 3,
-                                                  cmdtype=G3.ASTROPCI_COMMAND,
-                                                  cmdloc=G3.SSS,
-                                                  board=2, retval="DON")
-
-ARC_command_list["set_subframe_position"] = ARCDSPCmd('set_subframe_position',
-                                                      'simple', 2,
-                                                      cmdtype=G3.ASTROPCI_COMMAND,
-                                                      cmdloc=G3.SSP,
-                                                      board=2, retval="DON")
-
-ARC_command_list["set_image_parameters"] = ARCDSPCmd('set_image_parameters',
-                                                     'simple', 4,
-                                                     cmdtype=G3.ASTROPCI_COMMAND,
-                                                     cmdloc=G3.SIP,
-                                                     board=2, retval="DON")
-
-ARC_command_list["set_exposure_time"] = ARCDSPCmd('set_exposure_time',
-                                                  'simple', 1,
-                                                  cmdtype=G3.ASTROPCI_COMMAND,
-                                                  cmdloc=G3.SET,
-                                                  board=2, retval="DON")
-
-ARC_command_list["start_exposure"] = ARCDSPCmd('start_exposure',
-                                               'simple', 0,
-                                               cmdtype=G3.ASTROPCI_COMMAND,
-                                               cmdloc=G3.SEX,
-                                               board=2, retval="DON")
-
-ARC_command_list["clear_subframe"] = ARCDSPCmd('clear_subframe',
-                                               'simple', 0,
-                                               cmdtype=G3.ASTROPCI_COMMAND,
-                                               cmdloc=G3.CSB,
-                                               board=2, retval="DON")
-
-ARC_command_list["camera_open"] = ARCDSPCmd('camera_open',
-                                            'system', 0)
-
-ARC_command_list["camera_close"] = ARCDSPCmd('camera_close',
-                                             'system', 0)
-
-ARC_command_list["set_memory_map"] = ARCDSPCmd('set_memory_map',
-                                               'system', 1)
-
-ARC_command_list["initialize"] = ARCDSPCmd('initialize',
-                                           'system', 0)
-
-ARC_command_list["load_timing_dsp"] = ARCDSPCmd('load_timing_dsp',
-                                                'none', 1)
-
-ARC_command_list["get_timing_status"] = ARCDSPCmd('get_timing_status',
-                                                  'none', 0)
-
-ARC_command_list["sleep"] = ARCDSPCmd('sleep', 'system', 1)
-
-ARC_command_list["return_image"] = ARCDSPCmd('return_image', 'utilities', 3)
+ARCCmds["camera_open"] = DSPCmd('camera_open', 'system', 0)
+ARCCmds["camera_close"] = DSPCmd('camera_close', 'system', 0)
+ARCCmds["set_memory_map"] = DSPCmd('set_memory_map', 'system', 1)
+ARCCmds["initialize"] = DSPCmd('initialize', 'system', 0)
+ARCCmds["load_timing_dsp"] = DSPCmd('load_timing_dsp', 'none', 1)
+ARCCmds["get_timing_status"] = DSPCmd('get_timing_status', 'none', 0)
+ARCCmds["sleep"] = DSPCmd('sleep', 'system', 1)
+ARCCmds["return_image"] = DSPCmd('return_image', 'utilities', 3)
